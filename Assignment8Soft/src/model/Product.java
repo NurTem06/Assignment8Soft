5480c94dda03e03f98f4fbda1237f4ce1987aa2c
NurTem06 <temirgalinurislam73@gmail.com>
2025-11-13 17:08:01 +0500

    Delete Assignment8Soft directory

--- Assignment8Soft/src/model/Product.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import visitor.ProductVisitor;
-
-// Абстрактный интерфейс для всех продуктов
-public interface Product {
-    void accept(ProductVisitor visitor); // принимает посетителя
-}
